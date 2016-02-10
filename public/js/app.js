4f118d0924c067239bb3e49467e734af8eaba917
Kate Wood <katewood611@gmail.com>
2016-02-09 16:43:54 -0800

    adds custom directives for car form and navbar

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 angular.module('reviewApp', ['ui.router'])
+	.directive('navBar', navBar)
+	.directive('carForm', carForm)
+
+function carForm(){
+	var directive = {
+		restrict: 'E',
+		templateUrl: '/partials/car-form.html'
+	}
+	return directive
+}
+
+function navBar(){
+	var directive = {
+		restrict: 'E',
+		templateUrl: '/partials/nav.html',
+		transclude: true
+	}
+	return directive
+}
